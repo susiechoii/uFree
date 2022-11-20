b7d995cde7839d762f605854aeae19bc3dcaaee3
Leung Wai Liu <leungwai@wustl.edu>
2022-11-20 17:45:07 -0600

    imported Firestore

--- uFree/application/Application/AppDelegate.swift
+++ uFree/application/Application/AppDelegate.swift
@@ -1,14 +1,12 @@
-import Foundation
-import UIKit
-import Firebase
+import SwiftUI
+import FirebaseCore
+
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication
-                         .LaunchOptionsKey: Any]? = nil) -> Bool
-    {
-        FirebaseApp.configure()
-        return true
-    }
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
 }
